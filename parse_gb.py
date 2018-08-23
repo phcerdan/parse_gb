d4d99660ca1b6d41c14637a29e42b53b203a779d
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2018-08-22 23:34:02 -0400

    Add initial comments
    Remove `global` states, differentiating the input/output
    of the function to be re-usable.
    
    Add ability to run the script, or to import the function elsewhere with:
    ```
    if __name__ == "__main__":
    ```
    
    Read more:
    https://stackoverflow.com/questions/419163/what-does-if-name-main-do
    
    TODO:
    
    * Create a test with the fixture data to speed up development and
    correctness https://docs.python.org/3/library/unittest.html
    
    * Create an argument parser to not 'hardcode' any file and allow
    further testing.
    https://docs.python.org/3/howto/argparse.html

--- parse_gb.py
+++ parse_gb.py
@@ -1,30 +1,28 @@
 import re
 
+# TODO(PHC): DELETEME Nice documentation, should this be here, or inside the function?
 '''
 This script is for parsing a standard genbank file that contains multiple entries and returning a fasta file.
 It extracts the organism name and specimen voucher, and concatenates them
 into the first description field separated by a '_'. This is followed the associated
 accession number separated by ' '. The specimen sequences is written on the following line, in a single line. e.g.
-
-
 >Cortinarius_dulciolens_OTA60170 JX178605
-AAGTCGTAACAAGGTTTCCGTAGGTGAACCTGCGGAAGGATCATTATTGAAATAAACCTGATGAGTTGCTGCTGGTTCTCTAGGGAGCATGTGCACGCTTGTCATCTTTATATCTCCACCTGTGCACTTTGTGTAGACCTGGATATCTTTCTGAATGCCCTGCATTCAGGTTTGAGGATTGACTTTGCCGTCTTTCCTTACAATTCCGGGCCTATGTTTTTCATATTCCCCATAACCCCTGTTTATAGAATGTAATGAATGGGCCTTTGTGCCTATAAATCTTTATACAACTTTCAGCAACGGATCTCTTGGCTCTCGCATCGATGAAGAACGCAGCGAAATGCGATAAGTAATGTGAATTGCAGAATTCAGTGAATCATCGAATCTTTGAACGCACCTTGCGCTCCTTGGTATTCCGAGGAGCATGCCTGTTTGAGTGTCATTAATATATCAACCTCTTCAGATTTTTGTTTGTCGAGTGTTGGATGTGGGGGTCTTCCTTTTGCTGGCCTTTATTACTGAGGTCAGCTCCCCTGAAATGCATTAGCAGAACATTTTGTTGACTCGTTCATTGGTGTGATAACTATCTACGCTATTGACGTGGAAGCAACCCAAGTTCAGCTTCTAACAGTCTATTGATTTGGACAAAATTCTTTATTAATGTGACCTCAAATCAGGTAGGACTACCCGCTGAACTTAA
+AAGTCGTAACAAGGTTTC...
 >Cortinarius_dulciolens_PDD68471 NR_157914
-AACAAGGTTTCCGTAGGTGAACCTGCGGAAGGATCATTATTGAAATAAACCTGATGAGTTGCTGCTGGTTCTCTAGGGAGCATGTGCACGCTTGTCATCTTTATATCTCCACCTGTGCACTTTTTGTAGTCCTGGATATCTCTCTGAATGCTACCTAGCATTCAGGTATGAGGATTGACTTTGTAGTCTCTCCTTGCATTTCCAGGCCTATGTTTTTTCATATACCCATCCCCTGTTTATAGAATGTAATAAAATGGGCCTTTGTGCCTATAAACCTTTATACAACTTTCAGCAACGGATCTCTTGGCTCTCGCATCGATGAAGAACGCAGCGAAATGCGATAAGTAATGTGAATTGCAGAATTCAGTGAATCATCGAATCTTTGAACGCACCTTGCGCTCCTTGGTATTCCGAGGAGCATGCCTGTTTGAGTGTCATTAATATATCAACCTCTTCAGATTTTTTGTTTGTCGAGTGTTGGACGTGGGGGTTCTTTTGCTGGCCTTGAGGTCAGCTCCCCTGAAATGCATTAGCAGAACATTTTGTTAACCTGTTCATTGGTGTGATAACTATCTACGCTATTGACATGGAGCAACCAAGTTCAGCTTCCAACAGTCCATTGATTTGGACAAATTTTTCATTAATGTGACCTCAAATCAGGTAGGACTACCCGCTGAACTTA
-
-
+AACAAGGTTTCCGTAGGT...
 '''
 
+def formatted_sequence(input_filename, sequence_filename):
+    input_file = open(input_filename, 'r')
+    sequence_file = open(sequence_filename, 'w')
+    # TODO(PHC): DELETEME Are these PATTERNS something fixed, or change depending on the input?
+    # if the second, these should be arguments for the function.
+    # if the first, this is neat, please delete this comment
+    # also I like the CAPS for denoting CONSTANTS
+    SECTION_START_PATTERN = 'LOCUS'  # TODO: PHC: UNUSED, why?
+    SEQUENCE_START_PATTERN = 'ORIGIN'
+    END_PATTERN = '//'
 
-SECTION_START_PATTERN = 'LOCUS'
-SEQUENCE_START_PATTERN = 'ORIGIN'
-END_PATTERN = '//'
-
-input_file = open('sequence_single.gb','r')
-sequnce_file = open('formatted_seq.fasta', 'w')
-
-
-def formatted_seqence(input_file):
     match = False
     accession_number = ''
     organism = ''
@@ -49,7 +47,23 @@ def formatted_seqence(input_file):
             line = ''.join(line).upper()
             sequence += line
 
-            sequnce_file.write('>' + organism + '_' + specimen_voucher + ' ' + accession_number + '\n' + sequence)
+    sequence_file.write('>' + organism + '_' + specimen_voucher + ' ' + accession_number + '\n' + sequence)
 
 
-formatted_seqence(input_file)
+# TODO(PHC) DELETEME
+# we use this to allow execute this file as a script,
+# but also to be able to import it for other things with:
+# from parse_gb import (foo, bar)
+# see: https://stackoverflow.com/questions/419163/what-does-if-name-main-do
+if __name__ == "__main__":
+    """
+    Suggestion as future exercises:
+    1) IMPORTANT: create a test with the fixture data you have
+     this is super important and will allow you to be sure that any future change works, ease your mind when you change stuff (tests are passing)
+    see https://docs.python.org/3/library/unittest.html
+    2) parse arguments when you are not using test data anymore
+    see argparse https://docs.python.org/3/howto/argparse.html
+    """
+    inputFilename = 'sequence_multi.gb'
+    outputFilename = 'formatted_seq.fasta'
+    formatted_sequence(inputFilename, outputFilename)
